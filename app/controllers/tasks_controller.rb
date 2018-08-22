878efb9449f9847937cf6ea924e54508c888128c
yhomma <homma.yusaku@imagica.jp>
2018-08-22 05:59:24 +0000

    bug　fix about task CRUD

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
@@ -11,6 +12,7 @@ class TasksController < ApplicationController
   end
 
   def show
+    
   end
 
   def new
@@ -58,5 +60,12 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
